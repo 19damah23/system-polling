5da6b1a53789bcefcecff6ef5aab5e03d66f9034
Muchamad Agus Hermawan <muchamadagush@gmail.com>
2021-01-28 21:12:15 +0700

    create the polling system template

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="container">
+    <section class="vote">
+      <h3 class="give-your-vote">Give your vote of our services</h3>
+
+      <div class="emoticons">
+        //Ini akan menjadi element vote
+      </div>
+    </section>
+
+    <section class="finish">
+      <h4>Thank's for your vote</h4>
+    </section>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld
-  }
+  components: {}
 };
 </script>
+
+<style>
+  .container {
+    display: flex;
+    flex-wrap: wrap;
+    height: 100vh;
+    align-items: center;
+  }
+
+  section {
+    width: 100%;
+  }
+</style>
